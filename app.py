6039252833447c53ad88c6fe47dd77b54ed378d9
C418fan <116925179+C418fan@users.noreply.github.com>
2025-05-28 21:39:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -239,6 +239,7 @@ if (
     ))
 
     fig_portfolio_vs_ibov.update_layout(
+        title='COMPARAÇÃO: PORTFÓLIO REAL vs IBOVESPA (Base 100)',
         xaxis_title='Data',
         hovermode='x unified'
     )
